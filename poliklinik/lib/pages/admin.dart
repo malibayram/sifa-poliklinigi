0bc612c9fe7f111b3dce3066c06a6ba3cbf3f996
tgcsen <tugcessen@hotmail.com>
2021-05-16 23:37:12 +0300

    Update admin.dart

--- poliklinik/lib/pages/admin.dart
+++ poliklinik/lib/pages/admin.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 
 class Admin extends StatelessWidget {
+  String dropdownValue = 'One';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +19,29 @@ class Admin extends StatelessWidget {
             },
           ),
         ],
+      ),
+      body: DropdownButton<String>(
+        value: dropdownValue,
+        icon: const Icon(Icons.arrow_downward),
+        iconSize: 24,
+        elevation: 16,
+        style: const TextStyle(color: Colors.green),
+        underline: Container(
+          height: 2,
+          color: Colors.blue,
+        ),
+        onChanged: (String? newValue) {
+          {
+            dropdownValue = newValue!;
+          }
+        },
+        items: <String>['One', 'Two', 'Free', 'Four']
+            .map<DropdownMenuItem<String>>((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value),
+          );
+        }).toList(),
       ),
     );
   }
