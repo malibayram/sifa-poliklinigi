23caed6c5a94e9dd41a868901508fbb8e3dbb4f2
tgcsen <tugcessen@hotmail.com>
2021-05-16 20:37:10 +0300

    Update doktor.dart

--- poliklinik/lib/pages/doktor.dart
+++ poliklinik/lib/pages/doktor.dart
@@ -6,19 +6,63 @@ class Doktor extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Doktor Sayfası"),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.exit_to_app),
-            onPressed: () {
-              FirebaseAuth.instance
-                  .signOut()
-                  .then((value) => Hive.box('ayarlar').delete('personel'));
-            },
-          ),
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Doktor Sayfası"),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.exit_to_app),
+              onPressed: () {
+                FirebaseAuth.instance
+                    .signOut()
+                    .then((value) => Hive.box('ayarlar').delete('personel'));
+              },
+            ),
+          ],
+        ),
+        body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              children: [
+                Center(),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Hastanın Kimlik Numarası",
+                  ),
+                ),
+                SizedBox(height: 8),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Hastanın Adı - Soyadı",
+                  ),
+                ),
+                SizedBox(height: 8),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Hastanın Telefon Numarası",
+                  ),
+                ),
+                SizedBox(height: 8),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Hastanın Adresi",
+                  ),
+                ),
+                SizedBox(height: 8),
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 8.0,
+                      horizontal: 16.0,
+                    ),
+                    child: Text("Güncelle"),
+                  ),
+                )
+              ],
+            )));
   }
 }
