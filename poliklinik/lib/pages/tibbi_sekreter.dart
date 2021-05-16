46f67a4a7960105585f719ff5f0bcd4e51158a26
tgcsen <tugcessen@hotmail.com>
2021-05-16 23:06:07 +0300

    Update tibbi_sekreter.dart

--- poliklinik/lib/pages/tibbi_sekreter.dart
+++ poliklinik/lib/pages/tibbi_sekreter.dart
@@ -6,19 +6,42 @@ class TibbiSekreter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Tıbbi Sekreter Sayfası"),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.exit_to_app),
-            onPressed: () {
-              FirebaseAuth.instance
-                  .signOut()
-                  .then((value) => Hive.box('ayarlar').delete('personel'));
-            },
-          ),
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Tıbbi Sekreter Sayfası"),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.exit_to_app),
+              onPressed: () {
+                FirebaseAuth.instance
+                    .signOut()
+                    .then((value) => Hive.box('ayarlar').delete('personel'));
+              },
+            ),
+          ],
+        ),
+        body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              children: [
+                Center(),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Hasta Kimlik Numarası",
+                  ),
+                ),
+                SizedBox(height: 8),
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 8.0,
+                      horizontal: 16.0,
+                    ),
+                    child: Text("Hasta Ara"),
+                  ),
+                )
+              ],
+            )));
   }
 }
