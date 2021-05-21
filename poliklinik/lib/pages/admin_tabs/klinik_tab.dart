f91c2430fa1285e1918da5ad2fd3a0916f1ed147
tgcsen <tugcessen@hotmail.com>
2021-05-22 01:05:19 +0300

    Update klinik_tab.dart

--- poliklinik/lib/pages/admin_tabs/klinik_tab.dart
+++ poliklinik/lib/pages/admin_tabs/klinik_tab.dart
@@ -1,26 +1,90 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class KlinikTab extends StatelessWidget {
+  final _colRef = FirebaseFirestore.instance.collection('klinikler');
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
       stream: FirebaseFirestore.instance.collection('klinikler').snapshots(),
       builder: (context, snapshot) {
         if (snapshot.hasData) {
+          // ignore: unused_local_variable
           List<DocumentSnapshot> personeller = snapshot.data!.docs;
+          // ignore: unused_local_variable
+          Size screenSize = MediaQuery.of(context).size;
 
-          return ListView(
-            children: [
-              OutlinedButton(onPressed: () {}, child: Text("Yeni")),
-              Divider(),
-              for (DocumentSnapshot ds in personeller)
-                Card(
-                  child: ListTile(
-                    title: Text("${(ds.data() as Map)}"),
+          return Scaffold(
+            body: Row(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: LayoutBuilder(
+                    builder: (context, constraints) => Container(
+                      child: ListView(
+                        children: [
+                          Divider(),
+                          for (DocumentSnapshot ds in personeller)
+                            Card(
+                              child: ListTile(
+                                title: Text("${(ds.data() as Map)}"),
+                                trailing: IconButton(
+                                  icon: Icon(Icons.delete),
+                                  color: Colors.red,
+                                  onPressed: () {
+                                    _colRef.doc(ds.id).delete();
+                                  },
+                                ),
+                              ),
+                            ),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
-            ],
+                Expanded(
+                    flex: 3,
+                    child: LayoutBuilder(
+                      builder: (context, constraints) => Container(
+                        color: Colors.white,
+                        child: Column(
+                          children: [
+                            Center(),
+                            TextField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "Klinik ID:",
+                              ),
+                            ),
+                            TextField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "Klinik Adı:",
+                              ),
+                            ),
+                            TextField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "Klinik Tel No:",
+                              ),
+                            ),
+                            OutlinedButton(
+                              onPressed: () {
+                                _colRef.add({
+                                  'Klinik ID': "001",
+                                  'Klinik Adı': "Göz",
+                                  'Klinik Tel No': "02122121212",
+                                });
+                              },
+                              child: Text("Ekle"),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )),
+              ],
+            ),
           );
         } else
           return Center(child: CircularProgressIndicator());
