495d4289e1f8bee1ca9827b66d1d32e247888db5
tgcsen <tugcessen@hotmail.com>
2021-05-25 00:29:27 +0300

    Update tibbisekreter.dart

--- poliklinik/lib/models/tibbisekreter.dart
+++ poliklinik/lib/models/tibbisekreter.dart
@@ -1,13 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'doktor.dart';
 import 'randevu.dart';
+import 'hasta.dart';
 
 class TibbiSekreter {
-  String? isim;
-  String? soyisim;
-  String? telno;
+  List<Hasta> hastalar = [];
+  List<Doktor> doktorlar = [];
+  List<Randevu> randevular = [];
+  final _colHastaRef = FirebaseFirestore.instance.collection('hastalar');
+  final _colDoktorRef = FirebaseFirestore.instance.collection('doktorlar');
+  final _colPersonelRef = FirebaseFirestore.instance.collection('randevular');
 
-  List<Randevu>? randevular;
+  Future<List<Randevu>> randevulariListele() async {
+    return (await _colPersonelRef.get())
+        .docs
+        .map((ds) => Randevu.fromJson(ds.data()))
+        .toList();
+  }
+
+  Stream<List<Doktor>> tumDoktorlariAl() {
+    return _colDoktorRef
+        .snapshots()
+        .map((st) => st.docs.map((e) => Doktor.fromJson(e.data())).toList());
+  }
 
   randevuEkle(Randevu randevu) {
-    randevular?.add(randevu);
+    randevular.add(randevu);
+  }
+
+  randevuSil(Randevu randevu) {
+    randevular.remove(randevu);
   }
 }
