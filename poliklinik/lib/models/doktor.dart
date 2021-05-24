59b127267c7d64f31b2ff3df0984573d1de4294a
malibayram91 <malibayram91@gmail.com>
2021-05-24 14:24:41 +0300

    Klinik modeli muhtemelen tamamlandı

--- poliklinik/lib/models/doktor.dart
+++ poliklinik/lib/models/doktor.dart
@@ -1,14 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'islem.dart';
 import 'recete.dart';
 
 class Doktor {
+  String? id;
   String? isim;
   String? soyisim;
-  String? telno;
+  String? telNo;
 
   List<Recete>? receteler;
   List<Islem>? islemler;
-  
+
+  final _doktorColRef = FirebaseFirestore.instance.collection('doktorlar');
+
+  Doktor.fromJson(Map<String, dynamic>? json) {
+    this.isim = json?['isim'];
+  }
+
   receteEkle(Recete recete) {
     receteler?.add(recete);
   }
@@ -17,5 +26,20 @@ class Doktor {
     islemler?.add(islem);
   }
 
- 
+  Map<String, dynamic> toJson() {
+    return {
+      'id': this.id,
+      'isim': this.isim,
+      'soyisim': this.soyisim,
+      'tel-no': this.telNo,
+    };
+  }
+
+  Future<void> firebaseEkle() async {
+    await _doktorColRef.doc(this.id).set(this.toJson());
+  }
+
+  Future<void> firebasedenSil() async {
+    await _doktorColRef.doc(this.id).delete();
+  }
 }
--- poliklinik/lib/models/klinik.dart
+++ poliklinik/lib/models/klinik.dart
@@ -1,33 +1,68 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+import 'doktor.dart';
 
 class Klinik {
   String? id;
   String? adi;
   String? telNo;
+  final doktorlar = ValueNotifier<List<Doktor>>([]);
 
-  final _colRef = FirebaseFirestore.instance.collection('klinikler');
+  final _klinikColRef = FirebaseFirestore.instance.collection('klinikler');
 
   Klinik();
 
   Klinik.fromJson(Map json) {
-    this.id = json['id'];
-    this.adi = json['adi'];
-    this.telNo = json['tel-no'];
+    id = json['id'];
+    adi = json['adi'];
+    telNo = json['tel-no'];
+
+    final doktorColRef = FirebaseFirestore.instance.collection('doktorlar');
+
+    for (String dId in json['doktorlar']) {
+      if (doktorlar.value.where((e) => e.id == dId).length == 0) {
+        doktorColRef.doc(dId).get().then((value) {
+          doktorlar.value = [...doktorlar.value, Doktor.fromJson(value.data())];
+        });
+      }
+    }
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'id': this.id,
-      'adi': this.adi,
-      'tel-no': this.telNo,
+      'id': id,
+      'adi': adi,
+      'tel-no': telNo,
+      'doktorlar': doktorlar.value.map((e) => e.id).toList(),
     };
   }
 
+  Future<void> doktorEkle(Doktor doktor) async {
+    await doktor.firebaseEkle();
+
+    doktorlar.value = [...doktorlar.value, doktor];
+    await _klinikColRef.doc(id).update({
+      'doktorlar': FieldValue.arrayUnion([doktor.id])
+    });
+  }
+
+  Future<void> doktorSil(Doktor doktor) async {
+    await doktor.firebasedenSil();
+
+    doktorlar.value =
+        doktorlar.value.where((element) => element.id != doktor.id).toList();
+
+    await _klinikColRef.doc(id).update({
+      'doktorlar': FieldValue.arrayRemove([doktor.id])
+    });
+  }
+
   void firebaseEkle() {
-    _colRef.doc(this.id).set(this.toJson());
+    _klinikColRef.doc(id).set(toJson());
   }
 
   void firebasedenSil() {
-    _colRef.doc(this.id).delete();
+    _klinikColRef.doc(id).delete();
   }
 }
