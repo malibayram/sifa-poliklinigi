9fe381c826cc908337bb4016652d4f4b58312c3c
tgcsen <tugcessen@hotmail.com>
2021-05-25 00:29:08 +0300

    Update islem.dart

--- poliklinik/lib/models/islem.dart
+++ poliklinik/lib/models/islem.dart
@@ -1,7 +1,21 @@
 class Islem {
-
   String? islemadi;
   String? islemID;
   DateTime? islemTarihi;
 
+  Islem();
+
+  Islem.fromJson(Map<String, dynamic>? json) {
+    this.islemadi = json?['islem-adi'];
+    this.islemID = json?['islem-id'];
+    this.islemTarihi = json?['islem-tarihi'];
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'islem-id': this.islemID,
+      'islem-adi': this.islemadi,
+      'islem-tarihi': this.islemTarihi,
+    };
+  }
 }
