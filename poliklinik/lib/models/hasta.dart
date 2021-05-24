03ff6409c670a2f0d98b792758340a1310958c5d
tgcsen <tugcessen@hotmail.com>
2021-05-24 17:23:48 +0300

    Update hasta.dart

--- poliklinik/lib/models/hasta.dart
+++ poliklinik/lib/models/hasta.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:poliklinik/models/recete.dart';
 
 class Hasta {
   String? isim;
@@ -6,4 +9,35 @@ class Hasta {
   String? adres;
   DateTime? dogumtarihi;
 
+  final _hastaColRef = FirebaseFirestore.instance.collection('hastalar');
+
+  Hasta();
+
+  Hasta.fromJson(Map json) {
+    isim = json['isim'];
+    soyisim = json['soyisim'];
+    tcNo = json['tcNo'];
+    adres = json['adres'];
+    dogumtarihi = json['dogumtarihi'];
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'isim': isim,
+      'soyisim': soyisim,
+      'tcNo': tcNo,
+      'adres': adres,
+      'dogumtarihi': dogumtarihi,
+    };
+  }
+
+  Future<void> receteEkle(Recete recete) async {}
+
+  void firebaseEkle() {
+    _hastaColRef.doc(tcNo).set(toJson());
+  }
+
+  void firebasedenSil() {
+    _hastaColRef.doc(tcNo).delete();
+  }
 }
