1f1d21c902475e4a8b589cfae50d0e75073417a2
tgcsen <tugcessen@hotmail.com>
2021-05-25 14:46:06 +0300

    Update kayitgorevlisi.dart

--- poliklinik/lib/models/kayitgorevlisi.dart
+++ poliklinik/lib/models/kayitgorevlisi.dart
@@ -1,13 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'hasta.dart';
 
 class KayitGorevlisi {
-  String? isim;
-  String? soyisim;
-  String? telno;
-
   List<Hasta>? hastalar;
+  final _colHastaRef = FirebaseFirestore.instance.collection('hastalar');
 
   hastaEkle(Hasta hasta) {
+    hastalar?.add(hasta);
+  }
+
+  hastaSil(Hasta hasta) {
     hastalar?.add(hasta);
   }
 }
