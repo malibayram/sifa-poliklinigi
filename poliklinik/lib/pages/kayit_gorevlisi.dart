2127e3515a18f03a175badf202ac562b0eb7a156
tgcsen <tugcessen@hotmail.com>
2021-05-25 14:46:11 +0300

    Update kayit_gorevlisi.dart

--- poliklinik/lib/pages/kayit_gorevlisi.dart
+++ poliklinik/lib/pages/kayit_gorevlisi.dart
@@ -1,68 +1,109 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
+import 'package:poliklinik/models/hasta.dart';
 
 class KayitGorevlisi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Kayıt Görevlisi Sayfası"),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.exit_to_app),
-              onPressed: () {
-                FirebaseAuth.instance
-                    .signOut()
-                    .then((value) => Hive.box('ayarlar').delete('personel'));
-              },
-            ),
-          ],
-        ),
-        body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 400, vertical: 40),
-            child: Column(
-              children: [
-                Center(),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Hastanın Kimlik Numarası",
-                  ),
-                ),
-                SizedBox(height: 6),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Hastanın Adı - Soyadı",
-                  ),
-                ),
-                SizedBox(height: 6),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Hastanın Telefon Numarası",
-                  ),
-                ),
-                SizedBox(height: 6),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Hastanın Adresi",
-                  ),
-                ),
-                SizedBox(height: 8),
-                OutlinedButton(
-                  onPressed: () {},
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 8.0,
-                      horizontal: 16.0,
+    final hasta = Hasta();
+    return StreamBuilder<List<Hasta>>(
+        stream: hasta.tumBilgileriniAl(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            // ignore: unused_local_variable
+            Size screenSize = MediaQuery.of(context).size;
+
+            print(snapshot.data!);
+            return Scaffold(
+              body: Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: LayoutBuilder(
+                      builder: (context, constraints) => Container(
+                        child: ListView(
+                          children: [
+                            Divider(),
+                            for (Hasta hst in snapshot.data!)
+                              Card(
+                                child: ListTile(
+                                  title: Text("${hst.tcNo}"),
+                                  trailing: IconButton(
+                                    icon: Icon(Icons.delete),
+                                    color: Colors.red,
+                                    onPressed: hst.firebasedenSil,
+                                  ),
+                                ),
+                              ),
+                          ],
+                        ),
+                      ),
                     ),
-                    child: Text("Kaydet"),
                   ),
-                )
-              ],
-            )));
+                  Expanded(
+                    flex: 3,
+                    child: LayoutBuilder(
+                      builder: (context, constraints) => Container(
+                        color: Colors.white,
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 80, vertical: 40),
+                        child: Column(
+                          children: [
+                            Center(),
+                            TextField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "Hasta TC NO:",
+                              ),
+                              onChanged: (d) => hasta.tcNo = d,
+                            ),
+                            SizedBox(height: 8),
+                            TextField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "Hastanın İsimi:",
+                              ),
+                              onChanged: (d) => hasta.isim = d,
+                            ),
+                            SizedBox(height: 8),
+                            TextField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "Hastanın Soyisimi:",
+                              ),
+                              onChanged: (d) => hasta.soyisim = d,
+                            ),
+                            SizedBox(height: 8),
+                            TextField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "Klinik Adresi:",
+                              ),
+                              onChanged: (d) => hasta.adres = d,
+                            ),
+                            SizedBox(height: 8),
+                            TextField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "Hastanın Doğum Tarihi:",
+                              ),
+                              keyboardType: TextInputType.datetime,
+                              //   onChanged: (d) => hasta.dogumtarihi= d,
+                            ),
+                            SizedBox(height: 8),
+                            OutlinedButton(
+                              onPressed: hasta.firebaseEkle,
+                              child: Text("Ekle"),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          } else
+            return Center(child: CircularProgressIndicator());
+        });
   }
 }
