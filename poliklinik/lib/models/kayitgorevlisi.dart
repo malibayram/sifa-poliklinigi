c11280631f78fe74b5e05b756fbdf785f13b980c
tgcsen <tugcessen@hotmail.com>
2021-05-27 16:21:04 +0300

    Update kayitgorevlisi.dart

--- poliklinik/lib/models/kayitgorevlisi.dart
+++ poliklinik/lib/models/kayitgorevlisi.dart
@@ -1,10 +1,10 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'hasta.dart';
 
 class KayitGorevlisi {
   List<Hasta>? hastalar;
-  final _colHastaRef = FirebaseFirestore.instance.collection('hastalar');
+  // final _colHastaRef = FirebaseFirestore.instance.collection('hastalar');
 
   hastaEkle(Hasta hasta) {
     hastalar?.add(hasta);
