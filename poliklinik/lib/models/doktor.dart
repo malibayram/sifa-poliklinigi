2679cfa2307abeb3a645a68b2db3aa6cdf9833e7
malibayram91 <malibayram91@gmail.com>
2021-05-27 19:55:40 +0300

    boş

--- poliklinik/lib/models/doktor.dart
+++ poliklinik/lib/models/doktor.dart
@@ -1,27 +1,11 @@
-<<<<<<< HEAD
 import 'package:poliklinik/models/randevu.dart';
-=======
-//import 'package:cloud_firestore/cloud_firestore.dart';
->>>>>>> 95bfc6c5f3e7fc17a348a8b81e8e5d8f84bf8c36
 
 import 'hasta.dart';
 import 'personel.dart';
 
 class Doktor extends Personel {
-<<<<<<< HEAD
   List<Hasta> hastalar = [];
   List<Randevu> randevular = [];
-=======
-  String? id;
-  String? isim;
-  String? soyisim;
-  String? telNo;
-
-  List<Recete>? receteler;
-  List<Islem>? islemler;
-
-  //final _personelColRef = FirebaseFirestore.instance.collection('personeller');
->>>>>>> 95bfc6c5f3e7fc17a348a8b81e8e5d8f84bf8c36
 
   Doktor();
 
