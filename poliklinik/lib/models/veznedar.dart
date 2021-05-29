c3bf416e89777bfc08a50e99b8ed0c648e8edbc3
tgcsen <tugcessen@hotmail.com>
2021-05-29 21:36:26 +0300

    Update veznedar.dart

--- poliklinik/lib/models/veznedar.dart
+++ poliklinik/lib/models/veznedar.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'hasta.dart';
 import 'odeme.dart';
 
 class Veznedar {
@@ -6,6 +9,17 @@ class Veznedar {
   String? telNo;
 
   List<Odeme>? odemeler;
+
+  final _colHastaRef = FirebaseFirestore.instance.collection('hastalar');
+
+  Future<Hasta> hastaSorgula(String tcNo) async {
+    Hasta hasta = Hasta();
+    DocumentSnapshot<Map<String, dynamic>> hDoc =
+        await _colHastaRef.doc(tcNo).get();
+    if (hDoc.exists) hasta = Hasta.fromJson(hDoc.data()!);
+
+    return hasta;
+  }
 
   odemeAl(Odeme odeme) {
     odemeler?.add(odeme);
