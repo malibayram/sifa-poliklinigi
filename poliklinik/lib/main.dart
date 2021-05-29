61c4b33e06dfa0686fbd4ef52a6ed2f2370cac71
tgcsen <tugcessen@hotmail.com>
2021-05-29 21:36:23 +0300

    Update main.dart

--- poliklinik/lib/main.dart
+++ poliklinik/lib/main.dart
@@ -64,7 +64,7 @@ class MyApp extends StatelessWidget {
                       break;
 
                     case "veznedar":
-                      page = Veznedar();
+                      page = VeznedarPage();
                       break;
 
                     case "tekniker":
