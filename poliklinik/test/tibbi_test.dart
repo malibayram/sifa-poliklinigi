9b625b1d3420f69a3b994868caea92ab7dfa4e66
tgcsen <tugcessen@hotmail.com>
2021-05-31 10:28:27 +0300

    Update tibbi_test.dart

--- poliklinik/test/tibbi_test.dart
+++ poliklinik/test/tibbi_test.dart
@@ -1,25 +1,24 @@
 import 'dart:io';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hive/hive.dart';
 import 'package:poliklinik/models/tibbisekreter.dart';
 
+//Testi Gerçekleştiren : Tuğçe Şen
+// Tıbbi sekreter modelinde hastaSorgula() fonksiyonu için birim test yaptım.
+
 void main() async {
   TestWidgetsFlutterBinding.ensureInitialized();
-
   final path = Directory.current.path;
-
   Hive.init('$path/test');
-
   Box sorguBox = await Hive.openBox('hastalar');
 
   TibbiSekreter _tibbi = TibbiSekreter(testBox: sorguBox);
 
-  group('Hasta Sorgulama ve Randevu Oluşturma Testleri', () {
+  group('Hasta Sorgulama Testi', () {
     test("Hasta Sorgulama", () async {
       await _tibbi.hastaSorgula('111111');
-      String tibbi = sorguBox.get('tibbi', defaultValue: '');
-      expect(tibbi, '');
+      String hasta = sorguBox.get('hasta', defaultValue: '');
+      expect(hasta, '111111');
     });
   });
 }
